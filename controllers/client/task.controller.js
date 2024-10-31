78b2a481ed43d3f81649374a86c39ab0a4e4b4a1
hoahayhoi <hoa.tmh2003@gmail.com>
2024-10-31 08:46:37 +0700

    Task: filter by status and sort by criteria

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -1,17 +1,31 @@
 const Task = require("../../models/task.model");
 
 module.exports.index = async (req, res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
-    res.json(tasks);
+  const find = {
+    deleted: false
+  }
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  // Sort 
+  const sort = {};
+
+  if(req.query.sortKey && req.query.sortValue){
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  // End sort 
+
+  const tasks = await Task.find(find).sort(sort);
+  res.json(tasks);
 }
 
 module.exports.detail = async (req, res) => {
-    const id = req.params.id;
-    const task = await Task.findOne({
-      _id: id,
-      deleted: false
-    });
-    res.json(task);
-  }
+  const id = req.params.id;
+  const task = await Task.findOne({
+    _id: id,
+    deleted: false
+  });
+  res.json(task);
+}
