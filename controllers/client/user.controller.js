7d67e998b903d426de52012186473f27a70424f1
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-02 23:49:03 +0700

    reset password

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -133,10 +133,29 @@ module.exports.otpPassword = async (req, res) => {
     const user = await User.findOne({
         email: email
     });
-    
+
     res.json({
         code: "success",
         message: "Mã OTP hợp lệ!",
         token: user.token
+    });
+}
+
+
+module.exports.resetPassword = async (req, res) => {
+    const password = req.body.password;
+    const token = req.body.token;
+
+    await User.updateOne({
+        token: token,
+        // status: "active",
+        deleted: false
+    }, {
+        password: md5(password)
+    });
+
+    res.json({
+        code: "success",
+        message: "Đổi mật khẩu thành công!"
     });
 }
