13a18922a5e04e91ed41b78cc829ac8a8bc319b6
hoahayhoi <hoa.tmh2003@gmail.com>
2024-11-03 19:51:25 +0700

    Change routes into private

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,8 +1,9 @@
 const tasksRoute = require("./task.route");
 const userRoute = require("./user.route");
 
-module.exports = (app) => {
-    app.use("/tasks", tasksRoute);
+const userMiddleware = require("../../middlewares/client/user.middleware");
 
+module.exports = (app) => {
+    app.use("/tasks", userMiddleware.requireAuth, tasksRoute);
     app.use("/users", userRoute);
 }
